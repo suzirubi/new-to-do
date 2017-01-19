e6124f9547938b32e31930ec1df5e8b65f0718b8
Eric Raetz and Suzi Rubino <eric@>
2017-01-19 10:21:41 -0800

    add new task capability

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,11 +4,13 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { TaskListComponent } from './task-list.component';
 import { EditTaskComponent } from './edit-task.component';
+import { NewTaskComponent } from './new-task.component';
+
 
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
-  declarations: [AppComponent, TaskListComponent, EditTaskComponent ],
+  declarations: [AppComponent, TaskListComponent, EditTaskComponent, NewTaskComponent ],
   bootstrap: [ AppComponent ]
 })
 
